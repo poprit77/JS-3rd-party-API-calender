9b0ab2fee0603ad87cfe11c83da564ecc23183ae
unknown <peterataras@gmail.com>
2022-11-20 23:11:13 -0800

    finished code

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,165 @@
-// // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// // the code isn't run until the browser has finished rendering all the elements
-// // in the html.
-var description = document.querySelector("#description");
-var saveButton = document.getElementsByClassName('#saveBtn');
 
-// saveButton.addEventListener("click", function(event) {
-//   event.preventDefault();
+var description1 = document.getElementById("91");
+var description2 = document.getElementById("101");
+var description3 = document.getElementById("111");
+var description4 = document.getElementById("121");
+var description5 = document.getElementById("131");
+var description6 = document.getElementById("141");
+var description7 = document.getElementById("151");
+var description8 = document.getElementById("161");
+var description9 = document.getElementById("171");
+var saveButton = document.getElementsByClassName('saveBtn');
+saveButton[0].addEventListener("click", function (event) {
+  event.preventDefault();
+  savethis();
+});
+function savethis() {
+  var storage1 = {
+    description1: description.value
+  };
+  localStorage.setItem("storage1", JSON.stringify(storage1));
+}
+function renderLastGrade() {
+  var storage1 = JSON.parse(localStorage.getItem("storage1"));
+  if (storage1 !== null) {
+    description1.textContent = storage1.description;
+  }
+};
+saveButton[1].addEventListener("click", function (event) {
+  event.preventDefault();
+  savethis1();
+});
+function savethis1() {
+  var storage2 = {
+    description2: description2.value
+  };
+  localStorage.setItem("storage2", JSON.stringify(storage2));
+}
+function renderLastGrade1() {
+  var storage2 = JSON.parse(localStorage.getItem("storage2"));
+  if (storage2 !== null) {
+    description2.textContent = storage2.description2;
+  }
+};
 
-// var storage1 = {
-//   description: description.value
-// };
-// console.log(storage1)
-// localStorage.setItem("storage1", JSON.stringify(storage1));
-// var msgData = JSON.parse(localStorage.getItem("#description"));
-// save.textContent = save.firstName
+saveButton[2].addEventListener("click", function (event) {
+  event.preventDefault();
+  savethis2();
+});
+function savethis2() {
+  var storage3 = {
+    description3: description3.value
+  };
+  localStorage.setItem("storage3", JSON.stringify(storage3));
+}
+function renderLastGrade2() {
+  var storage3 = JSON.parse(localStorage.getItem("storage3"));
+  if (storage3 !== null) {
+    description3.textContent = storage3.description3;
+  }
+};
 
-// });
+saveButton[3].addEventListener("click", function (event) {
+  event.preventDefault();
+  savethis3();
+});
+function savethis3() {
+  var storage4 = {
+    description4: description4.value
+  };
+  localStorage.setItem("storage4", JSON.stringify(storage4));
+}
+function renderLastGrade3() {
+  var storage4 = JSON.parse(localStorage.getItem("storage4"));
+  if (storage4 !== null) {
+    description4.textContent = storage4.description4;
+  }
+};
+
+saveButton[4].addEventListener("click", function (event) {
+  event.preventDefault();
+  savethis4();
+});
+function savethis4() {
+  var storage5 = {
+    description5: description5.value
+  };
+  localStorage.setItem("storage5", JSON.stringify(storage5));
+}
+function renderLastGrade4() {
+  var storage5 = JSON.parse(localStorage.getItem("storage5"));
+  if (storage5 !== null) {
+    description5.textContent = storage5.description5;
+  }
+};
+
+saveButton[5].addEventListener("click", function (event) {
+  event.preventDefault();
+  savethis5();
+});
+function savethis5() {
+  var storage6 = {
+    description6: description6.value
+  };
+  localStorage.setItem("storage6", JSON.stringify(storage6));
+}
+function renderLastGrade5() {
+  var storage6 = JSON.parse(localStorage.getItem("storage6"));
+  if (storage6 !== null) {
+    description6.textContent = storage6.description6;
+  }
+};
+
+saveButton[6].addEventListener("click", function (event) {
+  event.preventDefault();
+  savethis6();
+});
+function savethis6() {
+  var storage7 = {
+    description7: description7.value
+  };
+  localStorage.setItem("storage7", JSON.stringify(storage7));
+}
+function renderLastGrade6() {
+  var storage7 = JSON.parse(localStorage.getItem("storage7"));
+  if (storage7 !== null) {
+    description7.textContent = storage7.description7;
+  }
+};
+
+saveButton[7].addEventListener("click", function (event) {
+  event.preventDefault();
+  savethis7();
+});
+function savethis7() {
+  var storage8 = {
+    description8: description8.value
+  };
+  localStorage.setItem("storage8", JSON.stringify(storage8));
+}
+function renderLastGrade7() {
+  var storage8 = JSON.parse(localStorage.getItem("storage8"));
+  if (storage8 !== null) {
+    description8.textContent = storage8.description8;
+  }
+};
+
+saveButton[8].addEventListener("click", function (event) {
+  event.preventDefault();
+  savethis8();
+});
+function savethis8() {
+  var storage9 = {
+    description9: description9.value
+  };
+  localStorage.setItem("storage9", JSON.stringify(storage9));
+}
+function renderLastGrade8() {
+  var storage9 = JSON.parse(localStorage.getItem("storage9"));
+  if (storage9 !== null) {
+    description9.textContent = storage9.description9;
+  }
+};
 
 
 //sets and splays current time 
@@ -23,27 +167,6 @@ var time1 = dayjs().format('MMMM  dddd ');
 var time2 = dayjs().format('DD ');
 $('#currentDay').text(time1 + "the " + time2);
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-//Controls time blocks background color with cascading if loop
 $(document).ready(readyFn);
 function readyFn(jQuery) {
   var hour = dayjs().format('HH');
@@ -103,3 +226,13 @@ function readyFn(jQuery) {
     $('#17').css({ 'background-color': '#d3d3d3' });
   }
 }
+renderLastGrade();
+renderLastGrade1();
+renderLastGrade2();
+renderLastGrade3();
+renderLastGrade4();
+renderLastGrade5();
+renderLastGrade6();
+renderLastGrade7();
+renderLastGrade8();
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-this website is designed to be a automatically updating calendar for the work day. Upon loading the user should see todays date displayed 
+this website is designed to be an automatically updating calendar for the work day. Upon loading the user should see todays date displayed below the title and description. The hour slots should automatically update the background color based on if the time slot is past, presesent or future. The user should be able to save information to the slot and reload the webiste and still have the data avaliable. 
+![A user clicks through an interactive coding quiz, then enters initials to save the high score before resetting and starting over.](./Assets/05-third-party-apis-homework-demo.gif)
